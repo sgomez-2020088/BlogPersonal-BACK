82293911cd656c3961bb330b41671c96eccc38b7
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-12 23:30:44 -0600

    se corrige funcion add

--- src/comment/comment.controller.js
+++ src/comment/comment.controller.js
@@ -1,21 +1,27 @@
 import Comment from '../comment/comment.model.js'
 import Post from '../post/post.model.js'
 
-export const addCommentary = async(req, res)=>{
-    try{
+export const addCommentary = async (req, res) => {
+    try {
+        
+        const { postId } = req.params
 
         const data = req.body
-        const comment = new Comment(data)
+        const comment = new Comment({
+            ...data, 
+            post: postId 
+        })
 
-        const post = await Post.findOne({ _id : data.post })
-        if (!post) return res.status(403).send({ succes : false, message: 'Post not found'})
+        const post = await Post.findById(postId)
+        if (!post) return res.status(403).send({ success: false, message: 'Post not found' })
 
         await comment.save()
-        return res.send({success : true, message: 'Comment added successfully'})
 
-    }catch(error){
+        return res.send({ success: true, message: 'Comment added successfully' })
+
+    } catch (error) {
         console.error(error)
-        return res.status(500).send({ success: false, message: 'General error saving the comment', error})
+        return res.status(500).send({ success: false, message: 'General error saving the comment', error })
     }
 }
 
@@ -54,5 +60,20 @@ export const deleteCommentary = async (req, res) => {
     } catch (error) {
         console.error(error)
         return res.status(500).send({ success: false, message: 'General error deleting commentary', error })
+    }
+}
+
+export const getCommentsByPostId = async (req, res) => {
+    try {
+        const { postId } = req.params 
+        const comments = await Comment.find({ post: postId }) 
+
+        if (comments.length === 0) return res.status(404).send({ success: false, message: 'No comments found for this post' })
+
+        return res.send({ success: true, comments }) 
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send({ success: false, message: 'General error getting comments', error })
     }
 }
